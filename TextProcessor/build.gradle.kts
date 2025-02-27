713dd5d370ba58846394af5c8280fa998b0f3b91
luotianyi <450905278@qq.com>
2025-02-27 15:18:24 +0800

    Initialize the project

---
+++ TextProcessor/build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    kotlin("jvm") version "2.1.10"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    maven { url = uri("https://mirrors.cloud.tencent.com/nexus/repository/maven-public/") }
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(17)
+}
